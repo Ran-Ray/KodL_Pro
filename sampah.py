846d5c8940b4658c0f9f609542ef7a248bf6a98a
Ran-Ray <139468378+Ran-Ray@users.noreply.github.com>
2023-08-30 10:38:18 +0700

    sampah.py

--- sampah.py
+++ sampah.py
@@ -15,3 +15,18 @@ def sampah_anorganik():
 
     return random.choice(nama_sampah)
 
+def perintah():
+    nama_perintah = ['organik', 'anorganik', 'joke', 'Info']
+
+    return (nama_perintah)
+
+def link():
+    links = ['https://www.thesca.org/connect/blog/how-recycle-and-why-you-should-do-it/',
+             'https://indonesiasustainability.com/pengertian-reduce-reuse-recycle/',
+             'https://www.universaleco.id/blog/detail/prinsip-3r-reduce-reuse-dan-recycle/156',
+             'https://hyundai.motorstudio.co.id/id/senayan-park/newsrooms/environmental-sustainability',
+             'https://icel.or.id/']
+
+    return random.choice(links)
+
+
