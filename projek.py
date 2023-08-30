c14e64c54ef874a9d985550190f9620c122f27fd
Ran-Ray <139468378+Ran-Ray@users.noreply.github.com>
2023-08-30 10:36:43 +0700

    projek.py

--- projek.py
+++ projek.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import os, random
-from sampah import sampah_anorganik, sampah_organik
+from sampah import sampah_anorganik, sampah_organik, link, perintah
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -21,5 +21,24 @@ async def organik(ctx):
 async def anorganik(ctx):
     await ctx.send('salah satu contoh sampah anorganik adalah :')
     await ctx.send(sampah_anorganik())
+
+@bot.command()
+async def joke(ctx):
+    img_name = random.choice(os.listdir('E memes'))
+    with open(f'E memes/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+        f.close()
+    await ctx.send(file=picture)
+
+@bot.command()
+async def usefull_link(ctx):
+    await ctx.send('salah satu link yang dapat membantu anda mempelajari tentang enviromental :')
+    await ctx.send(link())
+
+@bot.command()
+async def info(ctx):
+    await ctx.send('command :')
+    await ctx.send(perintah())
+    await ctx.send('bot by : Ran')
 
 bot.run("")
